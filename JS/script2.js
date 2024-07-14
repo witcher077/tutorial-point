3165d28eb4f3fc1945ee33f8d3ec68996524775b
witcher077 <ashokkumarmahto2000@gmail.com>
2024-07-14 14:19:15 +0530

    changes

--- JS/script2.js
+++ JS/script2.js
@@ -3,24 +3,26 @@ var ApiData = null;
 const product_container = document.getElementById('product-container');
 const store_search_box = document.getElementById('store_search_box');
 
-fetch("https://fakestoreapi.com/products")
-    .catch(err => console.log(err))
-    .then(d => d.json())
-    .then(data => {
-        ApiData = data;
-        console.log(ApiData);
-        let card;
-        store_search_box.addEventListener("change",(e)=>{
-            ApiData=data.filter(ele=>ele.title.toLowerCase().includes(e.target.value.toLowerCase()) )
-            console.log(ApiData);
+function FetchData() {
+
+    product_container.innerHTML = '<h1 class="loading">Loading.....</h1>';
+
+    fetch("https://fakestoreapi.com/products")
+        .catch(err => console.log(err))
+        .then(d => d.json())
+        .then(data => {
+            console.log(data);
+            ApiData = data;
+            displayData(ApiData);
         })
-        if (ApiData == null) {
-            card = '<h1 class="loading">Loading.....</h1>';
-            product_container.insertAdjacentHTML('beforeend', card)
-        }
-        else {
-            ApiData.forEach(element => {
-                card = ` <div class="card" style="background-color:rgb(241 245 249)">
+}
+FetchData();
+
+function displayData(products) {
+    if (products) {
+        product_container.innerHTML ='';
+        products.forEach(element => {
+            card = ` <div class="card" style="background-color:rgb(241 245 249)">
             <img style="border-radius: 10px;" width="220px" height="200px" src=${element.image} />
             <div class="details">
                 <h5 class="tittle">${element.title}</h5>
@@ -31,10 +33,10 @@ fetch("https://fakestoreapi.com/products")
         
             </div>
         </div>`;
-        product_container.insertAdjacentHTML('beforeend', card)
-            })
-        }
-        
-    })
+            product_container.innerHTML += card;
+        })
+    }
+}
 
+displayData(ApiData)
 
